5230ff3ba6a51b3e71b375e9cbb9c9e7558e47b6
Mykaela Saenz <mickycore@gmail.com>
2022-08-10 18:26:01 -0500

    fixed the delete button to actually delete

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const { notes } = require('./db/db.json');
+const { notes } = require('./db/db');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
 
+//parse icnoming string or array data
+app.use(express.urlencoded({ extended: true}));
+// parse incoming JSON data
+app.use(express.json());
 
-// // This part is used to get the user input and put it in the correct spot for notes
+// This part is used to get the user input and put it in the correct spot for notes
 function noteParts(query, notesArray) {
     let filteredResults = notesArray;
     if (query.title) {
@@ -34,7 +38,7 @@ function validateNotes(query) {
     if(!query.title || typeof query.title !== 'string') {
         return false;
     }
-    if(!query.body || typeof query.body !== 'string') {
+    if(!query.text || typeof query.text !== 'string') {
         return false;
     }
     return true;
@@ -54,10 +58,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-// This acts a a wildcard and will bring the users back to the homepage
-// if nothing is found that matches their request
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+app.get('/assets/js/index.js', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/assets/js/index.js'));
+});
+
+app.get('/assets/css/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/assets/css/styles.css'));
 });
 
 // This will redirect the user to the notes page.
@@ -69,19 +75,18 @@ app.post('/api/notes', (req, res) => {
     if (!validateNotes(req.body)) {
         res.status(400).send('This note is not properly formatted.');
     } else {
-        const notes = createNewNote(req.body, notes);
-        res.json(notes);
+        let noteArray = notes;
+        noteArray = createNewNote(req.body, noteArray);
+        res.json(noteArray);
     }
 });
 
-app.delete(`/api/notes:id`, (req, res) => {
-   deleteNote(id, notes);
+app.delete(`/api/notes/:id`, (req, res) => {
+   deleteNote(req.params.id, notes);
+   res.send();
 });
 
-//parse icnoming string or array data
-app.use(express.urlencoded({ extended: true}));
-// parse incoming JSON data
-app.use(express.json());
+
 
 
 app.listen(PORT, () => {
